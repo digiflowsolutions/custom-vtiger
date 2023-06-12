8f57fa766e1d33bce5d690997426058460d52472
POLETTI Stéphane <polettistephane@msn.com>
2023-06-12 14:48:35 +0200

    initial commit / create nav footer and main page

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  // Load navbar partial
+  $("#navbarPartial").load("components/navbar/navbar.html");
+
+  // Load footer partial
+  $("#footerPartial").load("components/footer/footer.html");
+});
