74353d2b8e2b28f9a7a21e31a8d9f5706528344c
POLETTI Stéphane <polettistephane@msn.com>
2023-06-19 15:22:40 +0200

    mini card contacts and comptes

--- js/comptes.js
+++ js/comptes.js
@@ -1,30 +1,100 @@
 fetch("data/comptes.json")
   .then((response) => response.json())
   .then((data) => {
-    // Sort the data array alphabetically by the 'Nom_compte' property
-    data.sort((a, b) => a.account_name.localeCompare(b.account_name));
-
+    data.sort((a, b) => a.accountname.localeCompare(b.accountname));
     const comptesContainer = document.getElementById("comptes-container");
 
+    // Create the search bar
+    const searchBar = document.createElement("input");
+    searchBar.setAttribute("type", "text");
+    searchBar.setAttribute("placeholder", "Search");
+    searchBar.classList.add("form-control", "mb-3");
+    comptesContainer.appendChild(searchBar);
+
     const boardList = document.createElement("div");
-    boardList.classList.add("list-group");
+    boardList.classList.add(
+      "row",
+      "row-cols-1",
+      "row-cols-md-2",
+      "row-cols-lg-4",
+      "g-4"
+    );
 
-    data.forEach((compte) => {
-      const link = document.createElement("a");
-      link.href = `compte.html?id=${compte.id}`;
-      link.textContent = compte.account_name;
-      link.classList.add("list-group-item", "list-group-item-action");
+    let filteredData = data; // Initially, set the filteredData to all contacts
 
-      link.addEventListener("mouseover", () => {
-        link.classList.add("list-group-item-success");
+    const renderComptes = () => {
+      boardList.innerHTML = ""; // Clear the existing contacts
+
+      filteredData.forEach((compte) => {
+        const cardColumn = document.createElement("div");
+        cardColumn.classList.add("col", "d-flex", "justify-content-center");
+
+        const link = document.createElement("a");
+        link.href = `contact.html?id=${compte.accountid}`; // Link to contact.html with contact ID
+        link.classList.add("list-group-item", "list-group-item-action");
+
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.style.maxWidth = "300px"; // Set maximum width for the card
+        card.style.margin = "auto"; // Add margin: auto to center the card
+
+        const imageContainer = document.createElement("div");
+        imageContainer.classList.add("card-img-container");
+        imageContainer.style.width = "100%";
+        imageContainer.style.height = "200px";
+        imageContainer.style.overflow = "hidden"; // Hide any overflowing content
+
+        const image = document.createElement("img");
+        image.src = compte.picture;
+        image.classList.add("card-img");
+        image.alt = "Profile Picture";
+        image.style.width = "100%"; // Make the image responsive
+
+        const cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
+
+        const title = document.createElement("h5");
+        title.classList.add("card-title");
+        title.innerHTML = `${compte.accountname.toUpperCase()}`;
+
+        const phone = document.createElement("p");
+        phone.classList.add("card-text");
+        phone.innerHTML = compte.mobile_phone;
+
+        const email = document.createElement("p");
+        email.classList.add("card-text");
+        email.innerHTML = compte.email_main;
+
+        cardBody.appendChild(title);
+        cardBody.appendChild(phone);
+        cardBody.appendChild(email);
+
+        imageContainer.appendChild(image);
+        card.appendChild(imageContainer);
+        card.appendChild(cardBody);
+
+        link.appendChild(card);
+        cardColumn.appendChild(link);
+        boardList.appendChild(cardColumn);
       });
+    };
 
-      link.addEventListener("mouseout", () => {
-        link.classList.remove("list-group-item-success");
-      });
+    const filterComptes = (event) => {
+      const searchTerm = event.target.value.toLowerCase();
 
-      boardList.appendChild(link);
-    });
+      // Filter the contacts based on the search term
+      filteredData = data.filter(
+        (compte) =>
+          compte.last_name.toLowerCase().includes(searchTerm) ||
+          compte.first_name.toLowerCase().includes(searchTerm)
+      );
+
+      renderComptes(); // Render the filtered contacts
+    };
+
+    searchBar.addEventListener("input", filterComptes);
+
+    renderComptes(); // Render all contacts initially
 
     comptesContainer.appendChild(boardList);
   })
--- js/contacts.js
+++ js/contacts.js
@@ -4,26 +4,99 @@ fetch("data/contacts.json")
     data.sort((a, b) => a.last_name.localeCompare(b.last_name));
     const contactsContainer = document.getElementById("contacts-container");
 
+    // Create the search bar
+    const searchBar = document.createElement("input");
+    searchBar.setAttribute("type", "text");
+    searchBar.setAttribute("placeholder", "Search");
+    searchBar.classList.add("form-control", "mb-3");
+    contactsContainer.appendChild(searchBar);
+
     const boardList = document.createElement("div");
-    boardList.classList.add("list-group");
+    boardList.classList.add(
+      "row",
+      "row-cols-1",
+      "row-cols-md-2",
+      "row-cols-lg-4",
+      "g-4"
+    );
 
-    data.forEach((contact) => {
-      const link = document.createElement("a");
-      link.href = `contact.html?id=${contact.id}`;
-      link.textContent =
-        contact.last_name.toUpperCase() + " " + contact.first_name;
-      link.classList.add("list-group-item", "list-group-item-action");
+    let filteredData = data; // Initially, set the filteredData to all contacts
 
-      link.addEventListener("mouseover", () => {
-        link.classList.add("list-group-item-success");
+    const renderContacts = () => {
+      boardList.innerHTML = ""; // Clear the existing contacts
+
+      filteredData.forEach((contact) => {
+        const cardColumn = document.createElement("div");
+        cardColumn.classList.add("col", "d-flex", "justify-content-center");
+
+        const link = document.createElement("a");
+        link.href = `contact.html?id=${contact.id}`; // Link to contact.html with contact ID
+        link.classList.add("list-group-item", "list-group-item-action");
+
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.style.maxWidth = "300px"; // Set maximum width for the card
+        card.style.margin = "auto"; // Add margin: auto to center the card
+
+        const imageContainer = document.createElement("div");
+        imageContainer.classList.add("card-img-container");
+        imageContainer.style.width = "100%";
+        imageContainer.style.height = "200px";
+        imageContainer.style.overflow = "hidden"; // Hide any overflowing content
+
+        const image = document.createElement("img");
+        image.src = contact.picture;
+        image.classList.add("card-img");
+        image.alt = "Profile Picture";
+        image.style.width = "100%"; // Make the image responsive
+
+        const cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
+
+        const title = document.createElement("h5");
+        title.classList.add("card-title");
+        title.innerHTML = `${contact.last_name.toUpperCase()} ${
+          contact.first_name
+        }`;
+
+        const phone = document.createElement("p");
+        phone.classList.add("card-text");
+        phone.innerHTML = contact.mobile_phone;
+
+        const email = document.createElement("p");
+        email.classList.add("card-text");
+        email.innerHTML = contact.email_main;
+
+        cardBody.appendChild(title);
+        cardBody.appendChild(phone);
+        cardBody.appendChild(email);
+
+        imageContainer.appendChild(image);
+        card.appendChild(imageContainer);
+        card.appendChild(cardBody);
+
+        link.appendChild(card);
+        cardColumn.appendChild(link);
+        boardList.appendChild(cardColumn);
       });
+    };
 
-      link.addEventListener("mouseout", () => {
-        link.classList.remove("list-group-item-success");
-      });
+    const filterContacts = (event) => {
+      const searchTerm = event.target.value.toLowerCase();
 
-      boardList.appendChild(link);
-    });
+      // Filter the contacts based on the search term
+      filteredData = data.filter(
+        (contact) =>
+          contact.last_name.toLowerCase().includes(searchTerm) ||
+          contact.first_name.toLowerCase().includes(searchTerm)
+      );
+
+      renderContacts(); // Render the filtered contacts
+    };
+
+    searchBar.addEventListener("input", filterContacts);
+
+    renderContacts(); // Render all contacts initially
 
     contactsContainer.appendChild(boardList);
   })
