9db6c5d43707aab7e43b39297e610cb724e1b3b1
POLETTI Stéphane <polettistephane@msn.com>
2023-06-27 14:30:31 +0200

    description div

--- modules/Description/description.js
+++ modules/Description/description.js
@@ -6,26 +6,42 @@ fetch("data/vtiger_crmentity.json")
     if (description) {
       const compteDescription = document.getElementById("compte-description");
 
-      const toggleButton = document.createElement("button");
-      toggleButton.className = "btn btn-secondary";
-      toggleButton.type = "button";
-      toggleButton.innerText = "Description";
+      const card = document.createElement("div");
+      card.className = "container w-100 m-auto";
+
+      const row = document.createElement("div");
+      row.className = "row";
+
+      const row1Column1 = document.createElement("div");
+      row1Column1.className =
+        "col-md-12 d-flex justify-content-center align-items-center m-auto ";
+
+      const toggleButton = document.createElement("div");
+      toggleButton.className =
+        "bg-secondary text-white rounded pl-5 pr-5 mt-3 mb-2 d-flex justify-content-between align-items-center";
+      //   toggleButton.type = "button";
+      toggleButton.innerText = "Description ";
       toggleButton.innerHTML += '<span class="arrow">&#9662;</span>'; // Add arrow HTML
 
       const descriptionDiv = document.createElement("div");
-      descriptionDiv.className = "description-content";
+      descriptionDiv.className = "description-content pl-3 mt-2";
       descriptionDiv.textContent = description.description;
 
       // Hide the description div by default
       descriptionDiv.style.display = "none";
 
       const toggleContainer = document.createElement("div");
-      toggleContainer.className = "toggle-container";
+      toggleContainer.className = "toggle-container w-100 m-auto";
       toggleContainer.appendChild(toggleButton);
       toggleContainer.appendChild(descriptionDiv);
 
+      row1Column1.appendChild(toggleContainer);
+      card.appendChild(row);
+
+      row.appendChild(row1Column1);
+
       // Append the toggle container to the compteDescription element
-      compteDescription.appendChild(toggleContainer);
+      compteDescription.appendChild(card);
 
       // Add click event listener to the toggle button
       toggleButton.addEventListener("click", function () {
@@ -41,7 +57,7 @@ fetch("data/vtiger_crmentity.json")
     } else {
       const errorElement = document.createElement("p");
       errorElement.classList.add("text-danger");
-      errorElement.textContent = "Compte inconnu";
+      errorElement.textContent = "";
 
       document.getElementById("compte-description").appendChild(errorElement);
     }
